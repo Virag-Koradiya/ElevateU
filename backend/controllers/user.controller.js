0b7ace29a13555b2c2dbd3622bec53919bb25272
chinmaiKewlani <chinmaikewlani27@gmail.com>
2025-09-22 19:12:45 +0530

    Bug

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,7 +13,7 @@ const makeCookieOptions = (maxAgeMs = 24 * 60 * 60 * 1000) => ({
 
 export const register = async (req, res) => {
     try {
-        const { fullname, email, phoneNumber, password, role } = req.body;
+        let { fullname, email, phoneNumber, password, role } = req.body;
          
         if (!fullname || !email || !phoneNumber || !password || !role) {
             return res.status(400).json({
